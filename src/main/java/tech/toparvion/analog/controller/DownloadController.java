a7a28008edf2f86894f0613816ce93d37e097bff
Toparvion <toparvion@gmx.com>
2018-04-21 16:48:29 +0700

    #10 Enriched backend download functionality

--- src/main/java/tech/toparvion/analog/controller/DownloadController.java
+++ src/main/java/tech/toparvion/analog/controller/DownloadController.java
@@ -3,8 +3,11 @@ package tech.toparvion.analog.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.PathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpClientErrorException;
@@ -13,21 +16,29 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 import tech.toparvion.analog.model.config.ClusterNode;
 import tech.toparvion.analog.model.config.ClusterProperties;
+import tech.toparvion.analog.service.DownloadRestTemplate;
 
+import javax.annotation.Nullable;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.Instant;
 
+import static java.lang.Math.max;
 import static java.lang.String.format;
-import static java.lang.String.valueOf;
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.nio.file.Files.isReadable;
-import static org.springframework.http.HttpHeaders.CONTENT_DISPOSITION;
+import static java.nio.file.Files.size;
+import static org.springframework.http.HttpHeaders.*;
 import static org.springframework.http.HttpStatus.*;
-import static org.springframework.http.MediaType.TEXT_PLAIN_VALUE;
+import static org.springframework.http.MediaType.TEXT_PLAIN;
 import static org.springframework.util.StringUtils.hasText;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.HEAD;
 
 /**
  * A controller for downloading currently selected logs and retrieving their sizes. Designed to be used both from
@@ -39,67 +50,114 @@ import static org.springframework.util.StringUtils.hasText;
 @RestController
 public class DownloadController {
   private static final Logger log = LoggerFactory.getLogger(DownloadController.class);
-  private static final String DOWNLOAD_SIZE_URI_PATH = "/download/size";
+  private static final String DOWNLOAD_URI_PATH = "/download";
 
   private final ClusterProperties clusterProperties;
+  private final DownloadRestTemplate downloadRestTemplate;
 
   @Autowired
   public DownloadController(ClusterProperties clusterProperties) {
     this.clusterProperties = clusterProperties;
+    this.downloadRestTemplate = new DownloadRestTemplate();
   }
 
-  @GetMapping(value = "/download", produces = TEXT_PLAIN_VALUE)
-  public ResponseEntity<Resource> downloadCurrentLog(@RequestParam("path") String pathParam,
-                                                     HttpServletResponse response) {
-    Path path = Paths.get(denormalize(pathParam));
-    if (!isReadable(path)) {
-      log.warn("Local file '{}' not found.", pathParam);
-      return new ResponseEntity<>(NOT_FOUND);
-    }
-    Resource logResource = new PathResource(path);
-    response.setHeader(CONTENT_DISPOSITION, format("attachment; filename=\"%s\"", path.getFileName().toString()));
-    return new ResponseEntity<>(logResource, OK);
-  }
-
-  @GetMapping(value = DOWNLOAD_SIZE_URI_PATH, produces = TEXT_PLAIN_VALUE)
-  public String getLogCurrentSize(@RequestParam("path") String pathParam,
-                                  @RequestParam(value = "node", required = false) String nodeName) throws IOException {
-    ClusterNode node = hasText(nodeName)
-        ? clusterProperties.findNodeByName(nodeName)
-        : clusterProperties.getMyselfNode();
-
+  @RequestMapping(value = DOWNLOAD_URI_PATH, method = GET)
+  public ResponseEntity<? extends Resource> downloadCurrentLog(
+      @RequestParam("path") String pathParam,
+      @RequestParam(value = "node", required = false) String nodeName,
+      @RequestParam(value = "last-mbytes", required = false, defaultValue = "0") int lastMBytes,
+      HttpServletResponse response) throws IOException
+  {
+    ClusterNode node = getNodeByName(nodeName);
     boolean isRemote = !node.equals(clusterProperties.getMyselfNode());
-    Long size;
+    if (!isRemote) {
+      Path path = Paths.get(denormalize(pathParam));
+      log.debug("Local file requested. Retrieving it from path: {}", path);
+      if (!isReadable(path)) {
+        throw new IllegalArgumentException(format("Local file '%s' not found or is inaccessible for reading.", path));
+      }
+      long entireFileSize = size(path);               // it's just a snapshot of file size which can change immediately
+      int lastBytes = lastMBytes << 20;               // 1 MByte = 2^20 bytes so it's enough to shift it left by 20
+      long readStartPosition = (lastBytes > 0)
+          ? max(0, (entireFileSize - lastBytes))      // 'max' to prevent exceeding of actual file size
+          : 0;
+      if (readStartPosition == 0) {
+        log.debug("Read start position is 0 so returning the file at whole...");
+        Resource pathResource = new PathResource(path);
+        response.setHeader(CONTENT_DISPOSITION, format("attachment; filename=\"%s\"", path.getFileName().toString()));
+        response.setHeader(CONTENT_TYPE, new MediaType(TEXT_PLAIN, UTF_8).toString());
+        return new ResponseEntity<>(pathResource, OK);
+
+      } else {
+        log.debug("Read start position is {} so returning files's tail only...", readStartPosition);
+        InputStream fileInputStream = Files.newInputStream(path);
+        long skipped = fileInputStream.skip(readStartPosition);
+        if (skipped != readStartPosition) {
+          log.warn("Actual skipped {} bytes while requested {} bytes.", skipped, readStartPosition);
+        }
+        InputStreamResource isr = new InputStreamResource(fileInputStream, format("resource based on '%s'", path));
+        response.setHeader(CONTENT_DISPOSITION, format("attachment; filename=\"%s\"", path.getFileName().toString()));
+        response.setHeader(CONTENT_LENGTH, String.valueOf(entireFileSize-skipped));
+        response.setHeader(CONTENT_TYPE, new MediaType(TEXT_PLAIN, UTF_8).toString());
+        return new ResponseEntity<>(isr, OK);
+      }
+    }
+
+    URI uri = UriComponentsBuilder.newInstance()
+        .scheme("http")
+        .host(node.getHost())
+        .port(clusterProperties.resolveServerPortFor(node))
+        .path(DOWNLOAD_URI_PATH)
+        .queryParam("path", pathParam)
+        .queryParam("last-mbytes", lastMBytes)
+        .build()
+        .toUri();
+    log.debug("Remote file requested. Retrieving it from URI: GET {}", uri);
+
+    return downloadRestTemplate.getForEntity(uri, InputStreamResource.class);
+  }
+
+  @RequestMapping(value = DOWNLOAD_URI_PATH, method = HEAD)
+  public HttpHeaders getLogCurrentSize(@RequestParam("path") String pathParam,
+                                       @RequestParam(value = "node", required = false) String nodeName) throws IOException {
+    ClusterNode node = getNodeByName(nodeName);
+    boolean isRemote = !node.equals(clusterProperties.getMyselfNode());
+    long size, lastModified;
     String extendedPath;
-    if (isRemote) {
-      RestTemplate restTemplate = new RestTemplate();
+    if (!isRemote) {
+      Path path = Paths.get(denormalize(pathParam));
+      log.debug("Local file size requested; Will query from path: {}", path);
+      if (!isReadable(path)) {
+        throw new IllegalArgumentException(format("Local file '%s' not found or is inaccessible for reading.", path));
+      }
+      size = size(path);
+      lastModified = Files.getLastModifiedTime(path).toMillis();
+      extendedPath = path.toAbsolutePath().toString();
+
+    } else {
       URI uri = UriComponentsBuilder.newInstance()
           .scheme("http")
           .host(node.getHost())
           .port(clusterProperties.resolveServerPortFor(node))
-          .path(DOWNLOAD_SIZE_URI_PATH)
+          .path(DOWNLOAD_URI_PATH)
           .queryParam("path", pathParam)
           .build()
           .toUri();
-      log.debug("Remote log size requested. Will query from URI: {}", uri);
-      String sizeStr = restTemplate.getForObject(uri, String.class);
-      if (!hasText(sizeStr)) {
-        throw new IllegalStateException(format("Empty result received from uri=%s", uri));
+      log.debug("Remote file size requested. Will query from URI: HEAD {}", uri);
+      HttpHeaders headers = new RestTemplate().headForHeaders(uri);
+      if (!headers.containsKey(CONTENT_LENGTH)) {
+        throw new IllegalStateException(format("No Content-Length received from uri=%s", uri));
       }
-      size = Long.valueOf(sizeStr);
+      size = headers.getContentLength();
+      lastModified = headers.getLastModified();
       extendedPath = uri.toString();
 
-    } else {
-      Path path = Paths.get(denormalize(pathParam));
-      if (!isReadable(path)) {
-        throw new IllegalArgumentException(format("Local file '%s' not found or is inaccessible for reading.", path));
-      }
-      size = Files.size(path);
-      extendedPath = path.toAbsolutePath().toString();
     }
-
-    log.debug("At the time of request, file '{}' had size {} bytes.", extendedPath, size);
-    return valueOf(size);
+    log.debug("File '{}' has size {} bytes, last modified {}.", extendedPath, size, Instant.ofEpochMilli(lastModified));
+    HttpHeaders httpHeaders = new HttpHeaders();
+    httpHeaders.setContentLength(size);
+    httpHeaders.setLastModified(lastModified);
+    return httpHeaders;
   }
 
   @ExceptionHandler(IOException.class)
@@ -111,7 +169,7 @@ public class DownloadController {
   @ExceptionHandler(IllegalArgumentException.class)
   @ResponseStatus(value = NOT_FOUND)
   public void handleLocalFileAbsence(IllegalArgumentException fileNotFoundException) {
-    log.error(fileNotFoundException.getMessage());
+    log.warn("Failed to download file: {} ", fileNotFoundException.getMessage());
   }
 
   @ExceptionHandler(HttpClientErrorException.class)
@@ -127,6 +185,12 @@ public class DownloadController {
   @ResponseStatus(value = BAD_GATEWAY)
   public void handleRemoteCallException(RestClientException remoteCallException) {
     log.error("Failed to retrieve size of remote file due to unexpected exception.", remoteCallException);
+  }
+
+  private ClusterNode getNodeByName(@Nullable String nodeName) {
+    return hasText(nodeName)
+        ? clusterProperties.findNodeByName(nodeName)
+        : clusterProperties.getMyselfNode();
   }
 
   private String denormalize(String pathParam) {
