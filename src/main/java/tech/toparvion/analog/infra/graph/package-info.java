df5df6b656c9e1fef884231c6ce224946c6f0021
Toparvion <toparvion@gmx.com>
2020-03-24 08:06:24 +0700

    Added servlet filter for detecting WebSocket connection establishment

---
+++ src/main/java/tech/toparvion/analog/infra/graph/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Additional classes for introspecting AnaLog's dynamic watching/tracking processes with the help of graph database
+ * 
+ * @author Toparvion
+ * @since v0.14
+ */
+package tech.toparvion.analog.infra.graph;
