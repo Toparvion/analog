18e3a83e453aba12e636e96845cc71ae9f6375f0
Toparvion <toparvion@gmx.com>
2020-05-09 08:36:17 +0700

    Revert "Added servlet filter for detecting WebSocket connection establishment"
    This reverts commit df5df6b6

--- src/main/java/tech/toparvion/analog/infra/graph/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Additional classes for introspecting AnaLog's dynamic watching/tracking processes with the help of graph database
- * 
- * @author Toparvion
- * @since v0.14
- */
-package tech.toparvion.analog.infra.graph;
